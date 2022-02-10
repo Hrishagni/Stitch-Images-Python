ec0ac8b3d3d370a05e9709e91864b119caa4c55f
Hrishagni De <hrishagni95@gmail.com>
2022-02-10 14:17:33 -0500

    Remove extra comments

--- task1.py
+++ task1.py
@@ -1,26 +1,3 @@
-"""
-Image Stitching Problem
-(Due date: Nov. 26, 11:59 P.M., 2021)
-
-The goal of this task is to stitch two images of overlap into one image.
-You are given 'left.jpg' and 'right.jpg' for your image stitching code testing. 
-Note that different left/right images might be used when grading your code. 
-
-To this end, you need to find keypoints (points of interest) in the given left and right images.
-Then, use proper feature descriptors to extract features for these keypoints. 
-Next, you should match the keypoints in both images using the feature distance via KNN (k=2); 
-cross-checking and ratio test might be helpful for feature matching. 
-After this, you need to implement RANSAC algorithm to estimate homography matrix. 
-(If you want to make your result reproducible, you can try and set fixed random seed)
-At last, you can make a panorama, warp one image and stitch it to another one using the homography transform.
-Note that your final panorama image should NOT be cropped or missing any region of left/right image. 
-
-Do NOT modify the code provided to you.
-You are allowed use APIs provided by numpy and opencv, except “cv2.findHomography()” and
-APIs that have “stitch”, “Stitch”, “match” or “Match” in their names, e.g., “cv2.BFMatcher()” and
-“cv2.Stitcher.create()”.
-If you intend to use SIFT feature, make sure your OpenCV version is 3.4.2.17, see project2.pdf for details.
-"""
 
 import cv2
 import numpy as np
@@ -47,13 +24,6 @@ def homography_calc(dist):
     return H
 
 def solution(left_img, right_img):
-    """
-    :param left_img:
-    :param right_img:
-    :return: you need to return the result panorama image which is stitched by left_img and right_img
-    """
-
-    # TO DO: implement your solution here
     r_rows=right_img.shape[0]
     r_cols=right_img.shape[1]
     l_rows=left_img.shape[0]
